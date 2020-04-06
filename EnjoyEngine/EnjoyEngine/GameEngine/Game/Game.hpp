d4d0848948a61761bd62bebbcb01e1bdd6ebd90a
Jar0T <jarotumula4@gmail.com>
2020-04-06 12:36:27 +0200

    Added Game files to GameEngine

---
+++ EnjoyEngine/EnjoyEngine/GameEngine/Game/Game.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <memory>
+
+#include "SFML/Graphics.hpp"
+#include "StateMachine.hpp"
+#include "Vectors.hpp"
+
+namespace EnjoyEngine {
+	struct GameData {
+		sf::RenderWindow window;
+		StateMachine machine;
+		Vector2D<int> windowSize;
+	};
+
+	typedef std::shared_ptr<GameData> GameDataRef;
+
+	class Game {
+	private:
+		sf::Clock _clock;
+		GameDataRef _data = std::make_shared<GameData>();
+
+		void Run();
+
+	public:
+		void Init(int wWidth, int wHeight, const char* wTitle);
+
+	};
+}
