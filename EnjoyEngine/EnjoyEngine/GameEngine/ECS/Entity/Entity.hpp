b9f585c46186dc21bd1f40271389529b7e092abd
Jar0T <jarotumula4@gmail.com>
2020-04-06 12:49:40 +0200

    Added Entity files to GameEngine ECS

---
+++ EnjoyEngine/EnjoyEngine/GameEngine/ECS/Entity/Entity.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <bitset>
+#include <memory>
+
+#include "Definitions.hpp"
+#include "EntityManager.hpp"
+#include "ComponentManager.hpp"
+
+namespace EnjoyEngine {
+	class Entity {
+	private:
+		std::bitset<MAX_COMPONENTS> _components;
+		bool _isActive = true;
+
+
+	public:
+		Entity() {
+			ID = EntityManager::get()->getID();
+		}
+
+		~Entity() {
+			ComponentManager::get()->deleteAllFromEntity(ID);
+			EntityManager::get()->entityDestroyed(ID);
+		}
+
+		template<typename T, typename... TArgs>
+		void addComponent(TArgs&&... mArgs) {
+			_components.set(getComponentTypeID<T>());
+			ComponentManager::get()->addComponent<T>(ID, std::forward<TArgs>(mArgs)...);
+		}
+
+		template<typename T>
+		bool hasComponent() {
+			return _components[getComponentTypeID<T>()];
+		}
+
+		template<typename T>
+		T& getComponent() {
+			return *ComponentManager::get()->getComponent<T>(ID);
+		}
+
+		template<typename T>
+		void removeComponent() {
+			_components.reset(getComponentTypeID<T>());
+			ComponentManager::get()->removeComponent<T>(ID);
+		}
+
+		bool isActive() {
+			return _isActive;
+		}
+
+		EntityID ID;
+
+	};
+}
---
+++ EnjoyEngine/EnjoyEngine/GameEngine/ECS/Entity/EntityManager.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <queue>
+
+#include "Definitions.hpp"
+
+namespace EnjoyEngine {
+	class EntityManager {
+	public:
+		static EntityManager* get() {
+			static EntityManager instance;
+			return &instance;
+		}
+
+		uint32_t getID() {
+			uint32_t ID = _freeIDs.front();
+			_freeIDs.pop();
+			entities.push_back(ID);
+			return ID;
+		}
+
+		void entityDestroyed(uint32_t ID) {
+			entities.erase(std::remove(entities.begin(), entities.end(), ID), entities.end());
+			_freeIDs.push(ID);
+		}
+
+		std::vector<uint32_t> entities;
+
+	private:
+		EntityManager() {
+			for (uint32_t i = 0; i < MAX_ENTITIES; i++) {
+				_freeIDs.push(i);
+			}
+		}
+
+		std::queue<uint32_t> _freeIDs;
+
+	};
+}
