e518e6ec3bd89047a5fe92348cc0c0eb8fc9b346
Jar0T <jarotumula4@gmail.com>
2020-04-09 14:32:04 +0200

    Added force usage to transform

--- EnjoyEngine/EnjoyEngine/Game/ECS/Components/TransformComponent.hpp
+++ EnjoyEngine/EnjoyEngine/Game/ECS/Components/TransformComponent.hpp
@@ -5,27 +5,33 @@
 namespace EnjoyEngine {
 	class TransformComponent : public Component {
 	public:
-		TransformComponent(float px = 0.f, float py = 0.f, float m = 10.f, float mVel = 10000.f) : RotationVelocity(0.f), Rotation(0.f), affectedByGravity(false), isStatic(false), mass(m), maxVelocity(mVel) {
+		TransformComponent(float px = 0.f, float py = 0.f, float m = 10.f, float mVel = 10000.f, bool grounded = true) : RotationVelocity(0.f), Rotation(0.f), affectedByGravity(false), isStatic(false), mass(m), maxVelocity(mVel), grounded(grounded) {
 			Position = { px, py };
 			Velocity = { 0.f, 0.f };
 			Acceleration = { 0.f, 0.f };
-			Gravity = { 0.f, (m * 9.81f) };
 		};
 
 		float mass;
 
 		bool affectedByGravity;
 		bool isStatic;
+		bool grounded;
 
 		Vector2D<float> Position;
 		Vector2D<float> Velocity;
 		Vector2D<float> Acceleration;
 
-		Vector2D<float> Gravity;
-
 		float maxVelocity;
 
 		float RotationVelocity;
 		float Rotation;
+
+		void addForce(Vector2D<float> force) {
+			Acceleration = Acceleration + force / mass;
+		}
+
+		void setForce(Vector2D<float> force) {
+			Acceleration = force / mass;
+		}
 	};
 }
--- EnjoyEngine/EnjoyEngine/Game/ECS/Systems/TransformSystem.hpp
+++ EnjoyEngine/EnjoyEngine/Game/ECS/Systems/TransformSystem.hpp
@@ -13,17 +13,29 @@ namespace EnjoyEngine {
 					bool isStatic = ComponentManager::get()->getComponent<TransformComponent>(entity)->isStatic;
 					//Move entity
 					if (!isStatic) {
+						float mass = ComponentManager::get()->getComponent<TransformComponent>(entity)->mass;
 						Vector2D<float> position = ComponentManager::get()->getComponent<TransformComponent>(entity)->Position;
 						Vector2D<float> velocity = ComponentManager::get()->getComponent<TransformComponent>(entity)->Velocity;
 						Vector2D<float> acceleration = ComponentManager::get()->getComponent<TransformComponent>(entity)->Acceleration;
+						if (velocity.magnitude() < 0.001)
+							velocity.setMagnitude(0);
+						Vector2D<float> friction = velocity * -1.f;
+						if (ComponentManager::get()->getComponent<TransformComponent>(entity)->grounded)
+							friction.setMagnitude(mass * 9.81 * 3.f);
+						else
+							friction.setMagnitude(0.f);
 						float maxVelocity = ComponentManager::get()->getComponent<TransformComponent>(entity)->maxVelocity;
-						position = position + velocity * deltaTime;
+						acceleration = acceleration + friction * deltaTime;
 						velocity = velocity + acceleration * deltaTime;
+						position = position + velocity * deltaTime;
+
 						if (ComponentManager::get()->getComponent<TransformComponent>(entity)->affectedByGravity) {
-							velocity = velocity + ComponentManager::get()->getComponent<TransformComponent>(entity)->Gravity * deltaTime;
+							acceleration.y = acceleration.y + 9.81f * deltaTime * deltaTime * mass;
 						}
+
 						ComponentManager::get()->getComponent<TransformComponent>(entity)->Position = position;
 						ComponentManager::get()->getComponent<TransformComponent>(entity)->Velocity = (velocity.magnitude() > maxVelocity ? velocity.setMagnitude(maxVelocity) : velocity);
+						ComponentManager::get()->getComponent<TransformComponent>(entity)->Acceleration.setMagnitude(0.f);
 					}
 
 					//Rotate entity
